6eb547ccc7dacf4e8ac6d20dddb8d66afc8a8ecf
Farman hafeez <farmanhafeezj@gmail.com>
2020-08-26 14:52:40 +0530

    Test code formatting

--- test.php
+++ test.php
@@ -5,21 +5,21 @@ header('Content-type: application/xml');
 require './jsonToXml.php';
 
 $data = '{
-    "contacts" : {
-       "contact" : {
-          "@attributes" : {
-             "id" : "1"
-          }, 
-          "name" : "John Doe", 
-          "phone" : "123-456-7890", 
-          "address" : {
-             "street" : "123 JFK Street", 
-             "city" : "Any Town", 
-             "state" : "Any State", 
-             "zipCode" : "12345"
-          }
-       }
+    "contacts": {
+        "contact": {
+            "@attributes": {
+                "id": "1"
+            },
+            "name": "John Doe",
+            "phone": "123-456-7890",
+            "address": {
+                "street": "123 JFK Street",
+                "city": "Any Town",
+                "state": "Any State",
+                "zipCode": "12345"
+            }
+        }
     }
- }';
+}';
 
 echo jsonToXml($data);
